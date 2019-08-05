7d34fcc468b3005976964b3ea413d8ab8a24f249
Siliang Chang <siliang.chang@gmail.com>
2019-08-04 22:37:33 -0600

    Block breaker part 1

---
+++ 4. Block Breaker/Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // configuration parameters
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Track mouse position in terms of Unity unit
+        // 16 is the total width of game camera 
+        // Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        float mousePosInUnit = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        // Creates a Vector2 to track x and y coordinates
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+
+        // Set the range of the x movement
+        paddlePos.x = Mathf.Clamp(mousePosInUnit, minX, maxX);
+
+        transform.position = paddlePos;
+    }
+}
