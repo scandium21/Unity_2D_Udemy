c848e8b6d9db9f74f623a0a5bf4437402e285798
Siliang Chang <siliang.chang@gmail.com>
2019-08-05 22:14:00 -0600

    Up to 'make a 2nd level'

---
+++ 4. Block Breaker/Block Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        // Show the name of the object collided into ME
+        // Debug.Log(collision.gameObject.name);
+    }
+
+}
