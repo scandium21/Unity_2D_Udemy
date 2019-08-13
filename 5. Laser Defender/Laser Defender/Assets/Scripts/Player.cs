2595fbb5c0aaa3d1db7c70fc8bcfe0afa624ed83
SC <siliang.chang@gmail.com>
2019-08-12 20:27:49 -0600

    Set movement boundaries for spaceship

--- 5. Laser Defender/Laser Defender/Assets/Scripts/Player.cs
+++ 5. Laser Defender/Laser Defender/Assets/Scripts/Player.cs
@@ -7,11 +7,30 @@ public class Player : MonoBehaviour
 {
     // config parameters
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.5f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    // cached reference
+    PlayerLaser plaser;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+        plaser = FindObjectOfType<PlayerLaser>();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -24,8 +43,9 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
 }
